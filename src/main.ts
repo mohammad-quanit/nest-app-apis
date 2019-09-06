63d1be00af59b30ef3f6b7ec0674d638e6c3ad9c
Mohammad-Quanit <muhammadquanit@gmail.com>
2019-09-06 12:38:20 +0500

    apis on nestjs with postgresql

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+import 'dotenv/config';
+import { Logger } from '@nestjs/common';
+
+const PORT = process.env.PORT || 8081;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+  Logger.log(`Server running on http://localhost:${PORT}`, 'Bootstrap');
+}
+bootstrap();
